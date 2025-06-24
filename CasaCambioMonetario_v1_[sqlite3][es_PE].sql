0ed13b15e6aefebf303f0869adc57b2b8692431a
Luis Carrillo Gutiérrez <carrilloluis@users.noreply.github.com>
2025-06-23 22:11:31 -0400

    Update CasaCambioMonetario_v1_[sqlite3][es_PE].sql

--- CasaCambioMonetario_v1_[sqlite3][es_PE].sql
+++ CasaCambioMonetario_v1_[sqlite3][es_PE].sql
@@ -13,3 +13,19 @@ INSERT INTO [Abreviatura Monetaria] VALUES (UPPER('eur'), 'europa', 'euro', '€
 INSERT INTO [Abreviatura Monetaria] VALUES (UPPER('usd'), 'ee.uu.', 'dólar', '$', 7);
 INSERT INTO [Abreviatura Monetaria] VALUES (UPPER('pen'), 'perú', 'nuevo sol', 'S/.', 7);
 
+DROP TABLE IF EXISTS [Fluctuación de dinero];
+CREATE TABLE IF NOT EXISTS [Fluctuación de dinero]
+(
+	[id] INTEGER NOT NULL,
+	-- posible fluctuación en el mismo día
+	[fecha de registro] DATETIME NOT NULL, -- DEFAULT CURRENT_TIMESTAMP
+	[moneda de origen] CHAR(3) NOT NULL REFERENCES [Abreviatura Monetaria](id),
+	[moneda de destino] CHAR(3) NOT NULL REFERENCES [Abreviatura Monetaria](id),
+	[tasa de cambio] DECIMAL(8, 3) NOT NULL CHECK([tasa de cambio] > 0.0),
+	[observaciones adicionales] TEXT NULL,
+	PRIMARY KEY ([id])
+) WITHOUT RowId;
+
+INSERT INTO [Fluctuación de dinero] VALUES (1920, '2014-09-12', 'USD', 'PEN', 2.80, NULL );
+INSERT INTO [Fluctuación de dinero] VALUES (1921, '2014-09-12', 'PEN', 'USD', (1 / 2.82), NULL );
+
